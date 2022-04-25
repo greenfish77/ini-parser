ce7976085f0a402d9860ca35eeb7ef6463fd33a2
greenfish <greenfish77@gmail.com>
2022-04-25 11:31:54 +0900

    dev

---
+++ include/headers.hpp
@@ -0,0 +1,35 @@
+#ifndef HEADER_HEADERS_HPP
+#define HEADER_HEADERS_HPP
+
+#ifdef _WIN32
+// windows version.
+#include <io.h>
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#else // _WIN32
+// linux version.
+#include <unistd.h>
+#include <sys/file.h>
+#endif // _WIN32
+#include <time.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+// C++17
+#include <string>
+#include <map>
+#include <vector>
+#include <mutex>
+#include <variant>
+#include <optional>
+
+#include "logger.hpp"
+#include "insert_order_map.hpp"
+#include "ini_parser.hpp"
+
+#endif // HEADER_HEADERS_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
-#include <stdio.h>
-#include <header.hpp>
+#include "headers.hpp"
 
-void main(void) {
+int main(int argc, const char** argv) try {
+	// log initialize.
+	init2(true);
 
+	info("***********");
+	info("* started *");
+	info("***********");
+
+	if (argc != 2) throw std::exception("ini_parser.exe <ini_file_path>.");
+
+	return 0;
+} catch (std::exception& e) {
+	std::string w = e.what();
+	error("<yellow>" + w + "</yellow>", true);
+	return 1;
 }
+
+// under windows, memory leak can be reported.
+#ifdef _WIN32
+static struct DUMP_MEMORY_LEAK {DUMP_MEMORY_LEAK() {_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF|_CRTDBG_LEAK_CHECK_DF);}} g_dml;
+#endif
